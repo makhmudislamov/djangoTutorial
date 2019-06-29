16e5f7a024d19ec0a79a11708973154acf2102f2
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-06-28 19:45:50 -0700

    added results

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from django.http import Http404
@@ -17,8 +16,8 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
